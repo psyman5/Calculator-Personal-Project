b45305b4224e3f3d7e4676db4c7b8aa76182a4ba
psyman5 <88164097+psyman5@users.noreply.github.com>
2021-07-29 16:06:56 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,41 @@
+import math
+operation = input("Operation: ")
+
+if operation=="dtrigonometry" or operation=="degreetrig" or operation=="dtrig" :
+    print("If value unknown, input 0.")
+    opp = float(input("Opposite: "))
+    hyp = float(input("Hypotenuse: "))
+    adj = float(input("Adjacent: "))
+    
+    if opp =="0" : 
+        oppzeropreAnswer = math.cos(adj/hyp)
+        math.acos(oppzeropreAnswer)
+        oppzeroAnswer = math.degrees(oppzeropreAnswer)
+        print(oppzeroAnswer)
+ 
+    if adj =="0" : 
+        adjzeropreAnswer = math.sin(opp / hyp)
+        math.asin(adjzeropreAnswer)
+        adjzeroAnswer = math.degrees(adjzeropreAnswer)
+        (print(adjzeroAnswer))    
+  
+    if hyp =="0" : 
+        hypzeropreAnswer = math.tan(opp/adj)
+        math.atan(hypzeropreAnswer)
+        hypzeroAnswer = math.degrees(hypzeropreAnswer)
+        print(hypzeroAnswer)
+else :
+    Number1 = float(input("Number 1: "))
+    Number2 = float(input("Number 2: "))
+    multiplicationAnswer = float(Number1 * Number2)
+    additionAnswer = float(Number1 + Number2)
+    divisionAnswer = float(Number1 / Number2)
+    subtractionAnswer = float(Number1 - Number2)
+    if operation=="*" : (print(multiplicationAnswer))
+
+    if operation=="+" : (print(additionAnswer))
+
+    if operation=="/" : (print(divisionAnswer))
+
+    if operation=="-" : (print(subtractionAnswer))
+
